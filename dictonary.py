f79fa6fa7aef97292c1b123b5e2b66a04b733d5f
kexinl124 <KExinl10@nycstudents.net>
2025-10-30 20:01:02 -0400

    Update dictonary.py

--- dictonary.py
+++ dictonary.py
@@ -18,51 +18,31 @@ total_items = [
 
 ]
 for index, item in enumerate(total_items):
-   print(index, ":", (item)["name"],(item)["price"],(item)["department"])
-   
+   print(index, ":", (item)["name"],item["price"])
 
+   print(f"Welcome to the store. Here are the items{total_items}")
    cart=[]
    choice=0
    total_cost=0
+   choice=input("Choose your item(s)")
+   
+   checkout=input("Do you wish to continue shopping?(yes/no)")
 
-   print("Welcome to store")
-   while choice != "done":
-      choose=(item)["name","price"]
-      choose=input("Choose what you want. When done say done:")
-      cart.append(choose)
-      print(f"You added {cart} into your cart")
-      if choice == "done":
-        print(f"Ok, you finish shopping. Here is your total cost{total_cost}")
-
-
-
-
-
-
-
-
-
-
-
-
-""" class Calculator():
-   def add(x,y):
-      print(x+y)
-      return x+y
-   def add_many(list):
-      print(sum(list))
-      return(sum(list))
-   def subtract(list):
-      return list
-Calculator.add(15,5) """
-""" class Hero:
-   def_init_(self,name,money,inventory):
-    self.name=name
-    self.money=money
-    self.inventory=inventory
-   def buy(self,item):
-     self.inventory.append(item)
-     print(f"{self.name} purchased {item} and has {self.item}")
-Nathan=Hero("Nathan",0,["Pencil"])
-print(Nathan._dict_)
-Nathan.buy("Xi Yang") """
+   while checkout!="no":
+        if choice==len(total_items):
+          cart.append(input(choice))
+          total_cost+=total_items[choice]["price"]
+          print(f"You added {cart} into your cart")
+          checkout=input("Do you wish to continue shopping?(yes/no)")
+   else:
+         print("Sorry, we do not have that item")
+   if checkout == "No":
+         break
+   for item in cart: 
+    print(f"{item['name']} ${item['price']}")
+    print(f"Total: ${total_cost}")
+   
+             
+         
+      
+   
