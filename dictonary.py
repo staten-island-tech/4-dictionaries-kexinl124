c17aefc06abefc2ab4c14d5e06cb8d8c688317fb
kexinl124 <KExinl10@nycstudents.net>
2025-11-07 08:46:58 -0500

    Update dictonary.py

--- dictonary.py
+++ dictonary.py
@@ -1,20 +1,25 @@
 total_items = [
 
+
        {"name": "water",
         "price": 2.00,
         "department": "liquid/beverages"},
+
 
        {"name": "orange juice",
       "price": 4.00,
       "department": "liquid/beverages"},
 
+
          {"name":"strawberry ",
          "price": 7.00,
          "department":"fruits"},
 
+
          {"name":"milk",
         "price": 5.00,
         "department":"liquid/beverages"}
+
 
 ]
 for index, item in enumerate(total_items):
@@ -29,18 +34,29 @@ print(f"You added {total_items[choice]['name']} into your cart")
 cost += total_items[choice]['price']
 
 
+
+
 while True:
       checkout=input("Do you wish to continue shopping?(yes/no)")
-      if checkout=="yes":
-               choice= int(input("What else would you like to buy?"))
-      print(f"You added {total_items[choice]['name']}into your cart")
-            
-      if checkout=="no":
-          break
 
-for item in cart: 
-      print(f"{(item['name'])} ${float(item['price'])}")
+
+      if checkout=="yes":
+            choice= int(input("What else would you like to buy?"))
+            cart.append(total_items[choice])
+            print(f"You added {total_items[choice]['name']}into your cart")
+            cost += total_items[choice]['price']
+      elif checkout == "no":
+            break
+      else:
+            print("say yes/no")
+
+
+
+
+for item in cart:
+      print(f"{(item['name'])}, ${float(item['price'])}")
 print(f"Total: ${cost}")
+
    
              
          
